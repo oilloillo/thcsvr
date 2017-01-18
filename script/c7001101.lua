de57b6a26e70abca85eb134ea5ec0683b15e7b9a
purerosefallen <78877@qq.com>
2017-01-18 12:21:48 +0800

    another

--- script/c7001101.lua
+++ script/c7001101.lua
@@ -15,17 +15,17 @@ function c7001101.filter(c)
 end
 function c7001101.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingTarget(c7001101.filter,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,c7001101.filter,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_COIN,g,1,0,0)
 end
 function c7001101.activate(e,tp,eg,ep,ev,re,r,rp)
 	local res=Duel.TossCoin(tp,1)
 	if res==1 then
-		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
 		Duel.Destroy(g,REASON_EFFECT)
 	else 
-		local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(Card.IsRelateToEffect,nil,e)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,0,LOCATION_MZONE,1,1,e:GetHandler())
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 	end
 end
