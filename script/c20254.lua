4a4087f015f7c131c5a8f46160979c82e298b4d3
shirone777 <799902937@qq.com>
2017-07-26 14:43:10 +0800

    1.976

--- script/c20254.lua
+++ script/c20254.lua
@@ -1,6 +1,6 @@
 --格杀勿论✿魂魄妖梦
 function c20254.initial_effect(c)
-	c:EnableUnsummonable()
+	--c:EnableUnsummonable()
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(20254,0))
@@ -10,12 +10,13 @@ function c20254.initial_effect(c)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCondition(c20254.spcon)
 	c:RegisterEffect(e1)
-	--summon,flip,special
+	--atk up
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetOperation(c20254.retreg)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(c20254.val)
 	c:RegisterEffect(e2)
 	--must attack
 	local e3=Effect.CreateEffect(c)
@@ -32,40 +33,6 @@ function c20254.spcon(e,c)
 	local ct=Duel.GetMatchingGroupCount(c20254.spfilter,c:GetControler(),LOCATION_GRAVE,0,nil)
 	return ct%2==1
 end
-function c20254.retreg(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	--to hand
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetDescription(aux.Stringid(20254,1))
-	e1:SetCategory(CATEGORY_TOHAND)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetProperty(EFFECT_FLAG_REPEAT)
-	e1:SetReset(RESET_EVENT+0x1ee0000+RESET_PHASE+PHASE_END)
-	e1:SetCondition(c20254.retcon)
-	e1:SetTarget(c20254.rettg)
-	e1:SetOperation(c20254.retop)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	c:RegisterEffect(e2)
-end
-function c20254.retcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsHasEffect(EFFECT_SPIRIT_DONOT_RETURN) then return false end
-	if e:IsHasType(EFFECT_TYPE_TRIGGER_F) then
-		return not c:IsHasEffect(EFFECT_SPIRIT_MAYNOT_RETURN)
-	else return c:IsHasEffect(EFFECT_SPIRIT_MAYNOT_RETURN) end
-end
-function c20254.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
-end
-function c20254.retop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		Duel.SendtoHand(c,nil,REASON_EFFECT)
-	end
+function c20254.val(e,c)
+	return c:GetEquipCount()*300
 end
